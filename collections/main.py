20df2a6edd9f04fe12ab4b961c5b4a5bb5bf830c
Williams Chen <chenwill2005@gmail.com>
2022-02-02 11:31:28 -0500

    test if nested list works, then convert to mongodb

--- collections/main.py
+++ collections/main.py
@@ -1,32 +1,58 @@
-import pymongo
+from datetime import datetime
+from pymongo import MongoClient
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
-import time
+from time import sleep
 
 link = ""
-client = pymongo.MongoClient()
+client = MongoClient(link)
 db = client.schools
 
 # apparently I need this to use ONE GOD DAMN DROPDOWN???
 PATH = "/Applications/chromedriver"
-options = webdriver.ChromeOptions()
-options.add_experimental_option("detach", True)
-driver = webdriver.Chrome(options=options, executable_path=PATH)
+driver = webdriver.Chrome(PATH)
 driver.get("https://www.nycenet.edu/PublicApps/Attendance.aspx")
 element = driver.find_element(By.ID,"ctl00_ContentPlaceHolder1_gvAttendance_ctl23_ddlPageSize")
 drp = Select(element)
-drp.select_by_index(3)
+drp.select_by_index(0)
 
 #bs4 right now it gets all of the table data and prints it 
 #TODO make it go into mongoDB
-tb1 = []
+dict1 = {}
 soup = BeautifulSoup(driver.page_source, 'html.parser')
-rows = soup.table.find_all("tr")
+rows = soup.find_all("td")
+now = datetime.today()
+date = now.strftime("%m/%d/%Y")
+
+
+result = [[] for _ in range(30)] 
+print(result)
+i = 0
+j = 0
+for row in rows:
+    if i == 0:
+        print(row.text)
+    result[i].append(row.text)
+    j+= 1
+    if j % 4 == 0:
+        i += 1
+
+print(result[1]==result[0])
+
 for row in rows:
     cells = row.find_all("td")
-    for i in cells:
-        print(i.text)
+    j = 0
+    for cell in cells:
+        if j == 0:
+            dict1['school_id'] = cell.text
+        elif j == 1:
+            dict1['school_name'] = cell.text
+            dict1[str(date)] = {}
+        elif j == 3:
+            dict1[str(date)]['percentage'] = cell.text
+        j+=1
+
 
 driver.quit()
