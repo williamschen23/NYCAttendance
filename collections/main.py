ce8a8a9b98fa0eaed6dcb687afaa21dffaa2054b
Williams Chen <chenwill2005@gmail.com>
2022-02-03 15:40:00 -0500

    Started MongoDB insertions TODO: figure out how to use aggreate and how to find population

--- collections/main.py
+++ collections/main.py
@@ -1,38 +1,44 @@
 from datetime import datetime
-from pymongo import MongoClient
+import pymongo
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
-from time import sleep
 
-link = ""
-client = MongoClient()
+# .env configuration
+from dotenv import load_dotenv
+from os import getenv
+load_dotenv()
+
+link = getenv('mongo_link')
+client = pymongo.MongoClient(link)
 db = client.schools
 
 # apparently I need this to use ONE GOD DAMN DROPDOWN???
 PATH = "/Applications/chromedriver"
 driver = webdriver.Chrome(PATH)
 driver.get("https://www.nycenet.edu/PublicApps/Attendance.aspx")
-element = driver.find_element(By.ID,"ctl00_ContentPlaceHolder1_gvAttendance_ctl23_ddlPageSize")
+element = driver.find_element(By.ID, "ctl00_ContentPlaceHolder1_gvAttendance_ctl23_ddlPageSize")
 drp = Select(element)
-drp.select_by_index(0)
+drp.select_by_index(3)
 
-#bs4 right now it gets all of the table data and prints it 
-#TODO make it go into mongoDB
-dict1 = {}
+# data
 soup = BeautifulSoup(driver.page_source, 'html.parser')
 rows = soup.find_all("td")
 now = datetime.today()
 date = now.strftime("%m/%d/%Y")
 
-result = [[] for _ in range(30)] 
+# init a data list and append all valid data for every row in it
+data = [[] for _ in range(int(len(rows)/4))]
 iterator = 0
 for j in range(len(rows)):
-    result[iterator].append(rows[j].text)
+    data[iterator].append(rows[j].text)
     j += 1
     if j % 4 == 0:
         iterator += 1
 
+# TODO make it go into mongoDB
 
+print(data)
 driver.quit()
+
